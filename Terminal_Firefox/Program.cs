774207965f0d7b9bfe4f30a484afa87d03b1185c
shariflatipov <sharif_limp@mail.ru>
2013-12-04 17:37:47 +0500

    Initial commit

---
+++ Terminal_Firefox/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+using Gecko;
+using NLog;
+
+namespace Terminal_Firefox {
+    static class Program {
+
+        private static Logger logger = LogManager.GetCurrentClassLogger();
+
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main() {
+            Xpcom.Initialize(@"xul");
+
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
