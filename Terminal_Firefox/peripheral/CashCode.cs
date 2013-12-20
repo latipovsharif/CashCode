b0d54e589446f466788ee17a6ca413792f63ddd5
Sharif Latipov <latipovsharif@gmail.com>
2013-12-20 17:05:55 +0500

    Cash code

--- Terminal_Firefox/peripheral/CashCode.cs
+++ Terminal_Firefox/peripheral/CashCode.cs
@@ -1,210 +1,148 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Windows.Forms;
-using System.IO;
 using System.IO.Ports;
 using System.Threading;
-using System.Runtime.InteropServices;
 
-namespace terminal
-{
-    public class CashCode
-    {   //готовые покеты
-        byte[] ConReACK = { 0x02, 0x03, 0x06, 0x00, 0xC2, 0x82 };
-        byte[] ConReset = { 0x02, 0x03, 0x06, 0x30, 0x41, 0xB3 };
-        byte[] ConGetSt = { 0x02, 0x03, 0x06, 0x31, 0xC8, 0xA2 };
-        byte[] ConStPoll = { 0x02, 0x03, 0x06, 0x33, 0xDA, 0x81 };
-        byte[] ConIdent = { 0x02, 0x03, 0x06, 0x37, 0xFE, 0xC7 };
-        byte[] ConGetBT = { 0x02, 0x03, 0x06, 0x41, 0x4F, 0xD1 };
-        byte[] ConStack = { 0x02, 0x03, 0x06, 0x35, 0xEC, 0xE4 };
-        byte[] ConReturn = { 0x02, 0x03, 0x06, 0x36, 0x77, 0xD6 };
-        byte[] ConHold = { 0x02, 0x03, 0x06, 0x38, 0x09, 0x3F };
-        byte[] ConExtBD = { 0x02, 0x03, 0x06, 0x3A, 0x1B, 0x1C };
-        byte[] ConReqSt = { 0x02, 0x03, 0x06, 0x60, 0xC4, 0xE1 };
-        byte[] ConEnBT = { 0x02, 0x03, 0x0C, 0x34, 0xFF, 0xFF, 0xFF, 0x00, 0x00, 0x00, 0xB5, 0xC1 };
-        byte[] ConDiBT = { 0x02, 0x03, 0x0C, 0x34, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x17, 0x0C };
+namespace Terminal_Firefox.peripheral {
+    public class CashCode {
+        //готовые покеты
+        private readonly byte[] _conReAck =  { 0x02, 0x03, 0x06, 0x00, 0xC2, 0x82 };
+        private readonly byte[] _conReset =  { 0x02, 0x03, 0x06, 0x30, 0x41, 0xB3 };
+        //private readonly byte[] _conGetSt =  { 0x02, 0x03, 0x06, 0x31, 0xC8, 0xA2 };
+        private readonly byte[] _conStPoll = { 0x02, 0x03, 0x06, 0x33, 0xDA, 0x81 };
+        //private readonly byte[] _conIdent =  { 0x02, 0x03, 0x06, 0x37, 0xFE, 0xC7 };
+        //private readonly byte[] _conGetBt =  { 0x02, 0x03, 0x06, 0x41, 0x4F, 0xD1 };
+        //private readonly byte[] _conStack =  { 0x02, 0x03, 0x06, 0x35, 0xEC, 0xE4 };
+        //private readonly byte[] _conReturn = { 0x02, 0x03, 0x06, 0x36, 0x77, 0xD6 };
+        //private readonly byte[] _conHold =   { 0x02, 0x03, 0x06, 0x38, 0x09, 0x3F };
+        //private readonly byte[] _conExtBd =  { 0x02, 0x03, 0x06, 0x3A, 0x1B, 0x1C };
+        //private readonly byte[] _conReqSt =  { 0x02, 0x03, 0x06, 0x60, 0xC4, 0xE1 };
+        private readonly byte[] _conEnBt =   { 0x02, 0x03, 0x0C, 0x34, 0xFF, 0xFF, 0xFF, 0x00, 0x00, 0x00, 0xB5, 0xC1 };
+        //private readonly byte[] _conDiBt =   { 0x02, 0x03, 0x0C, 0x34, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x17, 0x0C };
         
         //интервалы ожидания
-        int ReadIntervalTimeout = 400;
-        int ReadTotalTimeoutMultiplier = 0;
-        int ReadTotalTimeoutConstant = 400;
-        int WriteTotalTimeoutMultiplier = 0;
-        int WriteTotalTimeoutConstant = 30;
+        //private int _readIntervalTimeout = 400;
+        //private int _readTotalTimeoutMultiplier = 0;
+        //private int _readTotalTimeoutConstant = 400;
+        //private int _writeTotalTimeoutMultiplier = 0;
+        private const int WriteTotalTimeoutConstant = 30;
+
+        //номиналы купюр
+        public const byte B1 = 1;     // 0 0 0 0 0 0 0 1
+        public const byte B3 = 2;     // 0 0 0 0 0 0 1 0
+        public const byte B5 = 4;     // 0 0 0 0 0 1 0 0
+        public const byte B10 = 8;    // 0 0 0 0 1 0 0 0
+        public const byte B20 = 16;   // 0 0 0 1 0 0 0 0 
+        public const byte B50 = 32;   // 0 0 1 0 0 0 0 0
+        public const byte B100 = 64;  // 0 1 0 0 0 0 0 0
+        public const byte B200 = 128; // 1 0 0 0 0 0 0 0 
         
-        //номинали купюр
-        public const byte B1 = 1;      // 0 0 0 0 0 0 0 1
-        public const byte B3 = 2;      // 0 0 0 0 0 0 1 0
-        public const byte B10 = 4;     // 0 0 0 0 0 1 0 0
-        public const byte B50 = 8;     // 0 0 0 0 1 0 0 0
-        public const byte B100 = 16;   // 0 0 0 1 0 0 0 0 
-        public const byte B500 = 32;   // 0 0 1 0 0 0 0 0
-        public const byte B1000 = 64;  // 0 1 0 0 0 0 0 0
-        public const byte B5000 = 128; // 1 0 0 0 0 0 0 0 
         //переменная для ответа 
-        byte[] aAnswer;
-        //идентификатор запуска
-        bool start = false;
-        private int sum = 0;
-        //хендлер сомпорта
-        SerialPort port;
-
-        const int POLYNOMIAL = 0x08408;
-
-        private string msg;
+        private byte[] _aAnswer;
         
-        public CashCode()
-        {
-            port = new SerialPort("COM3", 9600, Parity.None, 8, StopBits.One);
-            //port.ReadBufferSize = 255;
-            //port.WriteBufferSize = 255;
-            port.Open();
-            port.WriteTimeout = 400;
-            port.ReadTimeout = 400;
+        //идентификатор запуска
+        private int _sum;
+        
+        //хендлер сом порта
+        private readonly SerialPort _port;
+        private const int POLYNOMIAL = 0x08408;
+        private string _msg;
 
+        public CashCode() {
+            _port = new SerialPort("com1", 9600, Parity.None, 8, StopBits.One);
+            _port.Open();
+            _port.WriteTimeout = 400;
+            _port.ReadTimeout = 400;
         }
 
-        public void Reset()
-        {
-            SendPacketInPort(ConReset);
+        public void Reset() {
+            SendPacketInPort(_conReset);
         }
 
-        public void ClosePort()
-        {
-            port.Close();
+        public void ClosePort() {
+            _port.Close();
         }
 
-        public void OpenPort()
-        {
-            if (port.IsOpen == false)
-            {
-                port.Open();
-                port.WriteTimeout = 400;
-                port.ReadTimeout = 400;
-            }
-
+        public void OpenPort() {
+            if (_port.IsOpen) return;
+            _port.Open();
+            _port.WriteTimeout = 400;
+            _port.ReadTimeout = 400;
         }
 
-        //private void button3_Click(object sender, EventArgs e)
-        //{
-        //    int msgsum;
-        //    start = true;
-        //    sum = 0;
-        //    msgsum = 0;
-        //    SendPacketInPort(ConEnBT);
-
-
-        //    while (start)
-        //    {
-        //        msg = "";
-        //        SendPacketInPort(ConStPoll);
-        //        ReadPort(out aAnswer, true);
-        //        if (aAnswer[2] == 7)
-        //        {
-        //            status(aAnswer[3], aAnswer[4], out msgsum, out msg);
-        //            if (msgsum > 0)
-        //            {
-        //                SendPacketInPort(ConReACK);
-        //                this.sum += msgsum;
-        //                msgsum = 0;
-        //            }
-        //        }
-        //    }
-        //}
-
-        public void ConEnBTMethod()
-        {
-            SendPacketInPort(ConEnBT);
+        public void ConEnBTMethod() {
+            SendPacketInPort(_conEnBt);
         }
 
-        public String[] MoneyPoll()
-        {
-            int msgsum = 0;
-            sum = 0;
-            msg = "";
+        public String[] MoneyPoll() {
+            _sum = 0;
+            _msg = "";
 
-            SendPacketInPort(ConStPoll);
-            if (ReadPort(out aAnswer, true) == true)
-            {
-                if (aAnswer[2] == 7)
-                {
-                    status(aAnswer[3], aAnswer[4], out msgsum, out msg);
-                    if (msgsum > 0)
-                    {
-                        SendPacketInPort(ConReACK);
-                        this.sum += msgsum;
-                        msgsum = 0;
+            SendPacketInPort(_conStPoll);
+            if (ReadPort(out _aAnswer)) {
+                if (_aAnswer[2] == 7) {
+                    Status(_aAnswer[3], _aAnswer[4], out _sum, out _msg);
+                    if (_sum > 0) {
+                        SendPacketInPort(_conReAck);
 
-                        return new String[2] { sum.ToString(), msg };
+                        return new String[2] { _sum.ToString(), _msg };
                     }
                 }
-            }
-            else
+            } else {
                 Reset();
-            return new String[2] { "", msg };
+            }
+
+            return new String[2] { "", _msg };
         }
 
-        
+
         //#############################################################\\
-        protected bool SendPacketInPort(byte[] aPacket)
-        {
-            try
-            {
-                port.WriteTimeout = WriteTotalTimeoutConstant;
-                port.DiscardOutBuffer();
-                port.Write(aPacket, 0, aPacket.Length);
+        protected bool SendPacketInPort(byte[] aPacket) {
+            try {
+                _port.WriteTimeout = WriteTotalTimeoutConstant;
+                _port.DiscardOutBuffer();
+                _port.Write(aPacket, 0, aPacket.Length);
             }
-            catch (Exception)
-            {
+            catch (Exception) {
                 return false;
             }
             return true;
         }
+
         //#############################################################\\
-        protected bool ReadPort(out byte[] aAnswer, bool aIsTimeOut)
-        {
+        protected bool ReadPort(out byte[] aAnswer) {
             Thread.Sleep(300);
-            aAnswer = new byte[port.BytesToRead];
-            try
-            {
-                port.Read(aAnswer, 0, aAnswer.Length);
+            aAnswer = new byte[_port.BytesToRead];
+            try {
+                _port.Read(aAnswer, 0, aAnswer.Length);
             }
-            catch (Exception)
-            {
+            catch (Exception) {
                 return false;
             }
 
             return true;
         }
 
-        
-        public static char calculateCrc16(byte[] data)
-        {
-            char crc = (char)0x0000;
-            int j;
+
+        public static char CalculateCrc16(byte[] data) {
+            char crc = (char) 0x0000;
             int count = (data[2] - 2);
-            if (count < 4) return (char)0;
-            for (int i = 0; i < count; i++)
-            {
-                crc ^= (char)data[i];
-                for (j = 0; j < 8; j++)
-                {
-                    if ((crc & 0x0001) == 1) { crc >>= 1; crc ^= (char)POLYNOMIAL; }
+            if (count < 4) return (char) 0;
+            for (int i = 0; i < count; i++) {
+                crc ^= (char) data[i];
+                for (int j = 0; j < 8; j++) {
+                    if ((crc & 0x0001) == 1) {
+                        crc >>= 1;
+                        crc ^= (char) POLYNOMIAL;
+                    }
                     else crc >>= 1;
                 }
             }
             return crc;
         }
 
-        
-        public bool status(byte first, byte second, out int money, out string msg)
-        {
-            switch (first)
-            {
+
+        public bool Status(byte first, byte second, out int money, out string msg) {
+            switch (first) {
                 case 0x10:
                 case 0x11:
                 case 0x12:
@@ -230,34 +168,28 @@ namespace terminal
                 case 0x41:
                     msg = "Полная кассета";
                     money = 0;
-                    start = false;
                     return false;
                 case 0x42:
                     msg = "Кассета отсутствует";
                     money = 0;
-                    start = false;
                     return false;
                 case 0x43:
                     msg = "Замяло купюру";
                     //reset
                     money = 0;
-                    start = false;
                     return false;
                 case 0x44:
                     msg = "Замяло касету 0_o";
                     //reset
                     money = 0;
-                    start = false;
                     return false;
                 case 0x45:
                     msg = "КАРАУЛ !!!! ЖУЛИКИ !!!";
                     //reset
                     money = 0;
-                    start = false;
                     return false;
                 case 0x47:
-                    switch (second)
-                    {
+                    switch (second) {
                         case 0x50:
                             msg = "Stack_motor_falure";
                             money = 0;
@@ -296,8 +228,7 @@ namespace terminal
                     msg = "";
                     return false;
                 case 0x1C:
-                    switch (second)
-                    {
+                    switch (second) {
                         case 0x60:
                             msg = "Insertion_error";
                             money = 0;
@@ -357,8 +288,7 @@ namespace terminal
                     return false;
 
                 case 0x81:
-                    switch (second)
-                    {
+                    switch (second) {
                         case 0:
                             msg = "Укладка";
                             money = 1;
@@ -407,6 +337,7 @@ namespace terminal
             }
 
         }
+
         //#############################################################\\
     }
 }
